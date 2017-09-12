ec6b323e75106aac93e46dac6f816d2752d71d97
Ravi Shankar Ram <ravishankar171196@gmail.com>
2017-09-12 21:03:08 +0530

    plot and code
    plot and code of PLOT 3

---
+++ plot3code.R
@@ -0,0 +1,19 @@
+data = read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+head(data)
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+plot3 <- function() {
+  plot(datetime,subMetering1, type="l", xlab="", ylab="Energy sub metering")
+  lines(datetime,subMetering2,col="red")
+  lines(datetime,subMetering3,col="blue")
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+  dev.copy(png, file="plot3.png", width=480, height=480)
+  dev.off()
+  cat("plot3.png has been saved in", getwd())
+}
+plot3()
+
